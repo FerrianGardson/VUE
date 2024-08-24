e976e70fc10f1608235da202773a291f840b943c
FerrianGardson <FerrianGardson@gmail.com>
2024-08-25 03:37:18 +0500

    Ошибка с {{ info }}

--- VUE/src/App.vue
+++ VUE/src/App.vue
@@ -1,10 +1,21 @@
-<script>
-</script>
-
 <template>
-  <h1>Hello world</h1>
+  <h1>{{ info }}</h1>
+  <p>{{ someInfo }}</p>
 </template>
 
 <style scoped>
-h1 {color:red}
+h1 {
+  color: red
+}
 </style>
+
+<script>
+export default {
+  dada() {
+    return {
+      info: 'Title!',
+      someInfo: 'Anons of message'
+    }
+  }
+}
+</script>
