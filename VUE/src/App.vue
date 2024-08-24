3b0f07bfd544cf0241de6956b07ed5df34150e40
FerrianGardson <FerrianGardson@gmail.com>
2024-08-25 04:47:04 +0500

    Ошибка

--- VUE/src/App.vue
+++ VUE/src/App.vue
@@ -1,6 +1,10 @@
 <template>
-  <h1>{{ info }}</h1>
-  <p>{{ someInfo }}</p>
+  <input type="text" v-model="userName" placeholder="Имя"></input>
+  <input type="password" v-model="userPass" placeholder="Пароль"></input>
+  <input type="email" v-model="userEmail" placeholder="Email"></input>
+  <button @click="sendData()">Отправить</button>
+  <p className="error"></p>
+  <p>{{ users }}</p>
 </template>
 
 <style scoped>
@@ -11,10 +15,31 @@ h1 {
 
 <script>
 export default {
-  dada() {
+  data() {
     return {
-      info: 'Title!',
-      someInfo: 'Anons of message'
+      users: [],
+      userName: '',
+      userPass: '',
+      userEmail: '',
+    }
+  },
+  methods: {
+    sendData() {
+      if (this.userName == '')
+        this.error = 'Вбейте имя'
+      return;
+      if (this.userPass == '')
+        this.error = 'Вбейте пароль'
+      return;
+      if (this.userEmail == '')
+        this.error = 'Вбейте почту'
+      return;
+
+      this.users.push({
+        name: this.userName,
+        pass: this.userPass,
+        emai: this.userEmail
+      })
     }
   }
 }
